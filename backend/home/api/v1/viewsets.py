0de540869dd8195785b427c45ec7ca95cc68b3e3
Crowdbotics <team@crowdbotics.com>
2021-10-06 20:38:52 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Student
+from .serializers import StudentSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,12 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    serializer_class = StudentSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Student.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,18 @@
+from django.conf import settings
 from django.db import models
+
+
+class Student(models.Model):
+    "Generated Model"
+    firstname = models.TextField()
+    email = models.EmailField(
+        max_length=254,
+    )
+    username = models.TextField(
+        null=True,
+        blank=True,
+    )
+    password = models.TextField(
+        null=True,
+        blank=True,
+    )
